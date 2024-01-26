08b9f5804b243683faa76a3f76060d23d259d08a
mulchus <mulch@list.ru>
2024-01-26 23:24:20 +0300

    Initial commit with some start views.

---
+++ main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+
+app = FastAPI()
+
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+
+
+class Items(BaseModel):
+    var1: int
+    var2: int
+    
+
+@app.get("/")
+def start():
+    return {"message2": "Hello World"}
+
+
+@app.post("/user/")
+def create_user(user: User):
+    return {"name": user.name.title(), "email": user.email}
+
+
+@app.post("/calc/add/")
+def add(items: Items):
+    return {
+        "a": items.var1,
+        "b": items.var2,
+        "sum": items.var1 + items.var2
+    }
+
+
+@app.post("/calc2/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "sum": a + b
+    }
+
+
+@app.get("/calc3/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "sum": a + b
+    }
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/items/")
+def list_items():
+    return [
+        "item1",
+        "item2",
+        "item3",
+    ]
+    
+    
+@app.get("/items/latest/")
+def read_last_item():
+    return {"item_id": "latest"}
+
+
+@app.get("/items/{item_id}/")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    
