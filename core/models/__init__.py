907dbd119cc1c2d452c8de82d78045f9b097a474
mulchus <mulch@list.ru>
2024-01-27 16:45:29 +0300

    на данном шаге приостановил знакомство, т.к. в видео в куче все. сразу сложно для изучения. видео № 3 # https://youtu.be/KWu_RyTKh1s?si=ynzhwhseaPZr1gWw&t=1201

---
+++ core/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = {
+    "Base",
+    "Product",
+}
+
+from .base import Base
+from .product import Product
+
+
+# на данном шаге приостановил знакомство, т.к. в видео в куче все. сразу сложно для изучения. видео № 3
+# https://youtu.be/KWu_RyTKh1s?si=ynzhwhseaPZr1gWw&t=1201
