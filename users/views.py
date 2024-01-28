ee48de5d5b67b3ff1b10a2316b1bb832b8289518
mulchus <mulch@list.ru>
2024-01-28 22:33:48 +0300

    add Multiple path and query parameters

--- users/views.py
+++ users/views.py
@@ -14,3 +14,17 @@ def create_user(user: User):
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}"}
+
+
+@router.get("/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
