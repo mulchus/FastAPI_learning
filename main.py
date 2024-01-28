aeeaefb3636b2b3d0094b742e776380d9ad14e76
mulchus <mulch@list.ru>
2024-01-28 20:31:39 +0300

    add read from file or list dir

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+import os.path
+
 from fastapi import FastAPI
 
 from calc_views import router as calc_router
 from item_views import router as item_router
 from users.views import router as user_router
-
+from model_views import router as model_router
 
 app = FastAPI()
 app.include_router(user_router, tags=["users"])
+app.include_router(model_router, tags=["models"])
 app.include_router(calc_router, tags=["calc"])
 app.include_router(item_router, prefix="/items-new", tags=["items-new"])
 
 
-@app.get("/")
-def start():
-    return {"message2": "Hello World"}
+@app.get("/", name="hello123")
+async def start(args: str = "World"):
+    names = [name.strip().title() for name in args.split()]
+    print(names)
+    return {"message": f"Hello {', '.join(names)}"}
+
+
+# получение информации из файла
+@app.get("/files/{file_path:path}")
+async def read_file_or_dir(file_path: str):
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as f:
+            return {"file_path": file_path, "data": f.read(200)}
+    elif os.path.isdir(file_path):
+        return {"file_path": file_path, "files": os.listdir(file_path)}
+    else:
+        return {"file_path": file_path, "exists": False}
 
 
 if __name__ == "__main__":
