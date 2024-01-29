bf46bf442e01d93cb26fcc9e83a04b08165ef1ad
mulchus <mulch@list.ru>
2024-01-29 23:19:45 +0300

    add "not  include_in_schema"

--- calc_views.py
+++ calc_views.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query
 from pydantic import BaseModel
 
 
@@ -13,27 +13,20 @@ class Items(BaseModel):
 
 @router.post("/add/")
 def add(items: Items):
-    return {
-        "a": items.var1,
-        "b": items.var2,
-        "sum": items.var1 + items.var2
-    }
+    return {"a": items.var1, "b": items.var2, "sum": items.var1 + items.var2}
 
 
 @router.post("2/add/")
-def add2(a: int, b: int):
-    return {
-        "a": a,
-        "b": b,
-        "sum": a + b
-    }
+def add2(
+    b: int,
+    a: Annotated[
+        int, Query(include_in_schema=False)
+    ] = 10,  # не отображать в схеме + по умолчанию 10
+):
+    return {"a": a, "b": b, "sum": a + b}
 
 
 @router.get("3/add/{a} {b}")
 # def add3(a: int, b: int):
 def add3(a: Annotated[int, Path(ge=0, le=1_000)], b: Annotated[int, Path(ge=0, le=10)]):
-    return {
-        "a": a,
-        "b": b,
-        "sum": a + b
-    }
+    return {"a": a, "b": b, "sum": a + b}
