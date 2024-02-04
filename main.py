32cc69a751bddb1946ea64d644b85c0ca0d1376a
mulchus <mulch@list.ru>
2024-02-04 23:15:45 +0300

    Multiple File Uploads with Additional Metadata

--- main.py
+++ main.py
@@ -132,12 +132,20 @@ async def login(username: Annotated[str, Form()], password: Annotated[str, Form(
 
 
 @app.post("/files/")
-async def create_files(files: Annotated[list[bytes], File()]):
+async def create_files(
+    # files: Annotated[list[bytes], File()]
+    files: Annotated[list[bytes], File(description="Multiple files as bytes")],
+):
     return {"file_sizes": [len(file) for file in files]}
 
 
 @app.post("/uploadfiles/")
-async def create_upload_files(files: list[UploadFile]):
+async def create_upload_files(
+    # files: list[UploadFile]
+    files: Annotated[
+        list[UploadFile], File(description="Multiple files as UploadFile")
+    ],
+):
     return {"filenames": [file.filename for file in files]}
 
 
