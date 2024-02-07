da7dc9c131103935ff7f46d8bd8bc68cb0917092
mulchus <mulch@list.ru>
2024-02-07 22:30:05 +0300

    add Dependencies with yield and HTTPException

--- main.py
+++ main.py
@@ -316,6 +316,33 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+class OwnerError(Exception):
+    pass
+
+
+data = {
+    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
+    "portal-gun": {"description": "Gun to create portals", "owner": "Rick"},
+}
+
+
+def get_username():
+    try:
+        yield "Rick"
+    except OwnerError as e:
+        raise HTTPException(status_code=400, detail=f"Owner error: {e}")
+
+
+@app.get("/items/yield-exc/{item_id}")
+def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+    if item_id not in data:
+        raise HTTPException(status_code=404, detail="Item not found")
+    item = data[item_id]
+    if item["owner"] != username:
+        raise OwnerError(username)
+    return item
+
+
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
